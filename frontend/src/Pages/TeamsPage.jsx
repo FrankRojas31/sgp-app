9b369aa5c577b8270fd08122b25a7728cacf2ad2
Gabriel <22393278@utcancun.edu.mx>
2024-01-29 22:43:14 -0500

    Detallitos de equipos

--- frontend/src/Pages/TeamsPage.jsx
+++ frontend/src/Pages/TeamsPage.jsx
@@ -1,32 +1,18 @@
 import styles from '../css/targetTeam.module.css';
 import { Sidebar } from '../Components/dashboard/Sidebar';
 import TargetTeam from '../Components/targetTeam';
+
 function TeamsPage() {
-
-
   return (
-    <div>
+    <div className={styles.pageContainer}> {/* Contenedor Principal */}
       <Sidebar />
-      <div className={styles.content}>
-        <h1>
-          EQUIPOS
-        </h1>
-
-        <button className={styles.botonagregar}>
-          Agregar
-        </button>
-
+      <div className={styles.mainContainer}> {/* Contenido Principal */}
+        <h1>EQUIPOS</h1>
+        <button className={styles.botonagregar}> Agregar </button>
         <TargetTeam />
-
       </div>
-
     </div>
   );
 }
 
 export default TeamsPage;
-
-
-
-
-
