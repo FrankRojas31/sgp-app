c7963dc3ecd53e3b6306999b8afc2698920898f4
SublimeDevv <juanmen1404@gmail.com>
2024-02-05 16:01:32 -0500

    Se añaden permisos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/notfound" element={<ErrorPage />} />
         <Route element={<PrivateRoute allowRoles={["admin"]} />}>
           <Route path="/members" element={<MembersPage />} />
